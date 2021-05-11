4780d180b0406cd54884ea6cde8077f580a6c3b1
Anthourn <80663362+Anthourn@users.noreply.github.com>
2021-05-11 15:12:53 -0400

    render games

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,39 @@
 class App extends React.Component{
+    state = {
+        title:"",
+        year:"",
+        genre:"",
+        review:"",
+        image:"",
+        games:[]
+
+
+    }
+    componentDidMount = () => {
+        axios.get('/games').then((response) =>{
+            this.setState({games:response.data})
+        })
+    }
     render = () => {
         return <div>
         <h1>Testing</h1>
+        <div>
+        <ul>
+        {this.state.games.map(
+            (game =>{
+                return (
+                    <li key = {game._id}>
+                    {game.title}
+                    {game.genre}
+                    {game.year}
+                    {game.image}
+                    {game.review}
+                    </li>
+                )
+            })
+        )}
+        </ul>
+        </div>
         </div>
     }
 }

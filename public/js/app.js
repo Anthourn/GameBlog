d823d685912e20cb444faeb9b0451b8623ddfc7c
Heath Friend <heathfriend@heaths-air.home>
2021-05-12 10:30:08 -0400

    routes working properly

--- public/js/app.js
+++ public/js/app.js
@@ -13,6 +13,43 @@ class App extends React.Component{
     handleChange = (event) => {
         this.setState({[event.target.id]: event.target.value})
     }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post('/games', this.state).then((response) => {
+            this.setState({games: response.data,
+                title: '',
+                year:'',
+                genre:'',
+                review:'',
+                image:'',
+            })
+        })
+    }
+
+    updateGame = (event) => {
+        event.preventDefault()
+        const id = event.target.id
+        axios.put('/games/' + id, this.state).then((response) => {
+            this.setState({
+                games: response.data,
+                title: '',
+                year:'',
+                genre:'',
+                review:'',
+                image:'',
+            })
+        })
+
+    }
+
+    deleteGame = (event) => {
+        axios.delete('/games/' + event.target.value).then(response => {
+            this.setState({
+                games: response.data,
+            })
+        })
+    }
     componentDidMount = () => {
         axios.get('/games').then((response) =>{
             this.setState({games:response.data})
@@ -21,23 +58,45 @@ class App extends React.Component{
     render = () => {
         return <div>
         <h1>Testing</h1>
-        <div>
+        <form onSubmit={this.handleSubmit}>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" onChange={this.handleChange} />
+            <br/>
+            <label htmlFor="image">Image</label>
+            <input type="text" id="image" onChange={this.handleChange}
+            value={this.state.image} />
+            <br/>
+            <input type="submit" value="Create Game"/>
+        </form>
+
         <ul>
         {this.state.games.map(
             (game =>{
                 return (
                     <li key = {game._id}>
-                    {game.title}
-                    {game.genre}
-                    {game.year}
-                    <img src ={game.image} alt = {game.title}/>
+                    {game.title}<br/>
+                    {game.genre}<br/>
+                    {game.year}<br/>
+                    <img src ={game.image} alt = {game.title}/><br/>
                     {game.review}
+                        <summary>Edit Game</summary>
+                        <form id={game._id} onSubmit={this.updateGame}>
+                            <label htmlFor="title">Title</label>
+                            <br/>
+                            <input type="text" id="title" onChange={this.handleChange}/>
+                            <label htmlFor="image">Image</label>
+                            <br/>
+                            <input type="text" id="image" onChange={this.handleChange} />
+                            <br/>
+                            <input type="submit" value="Update Game"/>
+                        </form>
+                        <button value={game._id} onClick={this.deleteGame}>
+                        DELETE </button>
                     </li>
                 )
             })
         )}
         </ul>
-        </div>
         </div>
     }
 }
